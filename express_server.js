601aeefc3f9a337a22211aaa580b6e7279b8e792
animward <animward@gmail.com>
2024-03-18 13:43:06 -0700

    added functionality to generateRandomString function

--- express_server.js
+++ express_server.js
@@ -60,4 +60,11 @@ app.get("/u/:id", (req, res) => {
     }
 });
 
-function generateRandomString() {}
+function generateRandomString(length = 6) {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let randomString = '';
+    for (let i = 0; i < length; i++) {
+        randomString += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return randomString;
+}
