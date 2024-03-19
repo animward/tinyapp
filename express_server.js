2d5201902c8ef4adad22a0cea88d6bedbc037736
animward <animward@gmail.com>
2024-03-19 13:28:05 -0700

    added comments, stress tested, and made edit button more user friendly

--- express_server.js
+++ express_server.js
@@ -9,28 +9,34 @@ app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 
+// url database
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+// root test
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+// listen
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+// render new URL form
 app.get("/urls/new", (req, res) => {
     res.render("urls_new");
 });
 
+// display URL list
 app.get("/urls", (req, res) => {
     let templateVars = { urls: urlDatabase };
     res.render("urls_index", templateVars);
   });
 
+// display URL details
 app.get("/urls/:id", (req, res) => {
     const id = req.params.id;
     const longURL = urlDatabase[id];
@@ -38,14 +44,17 @@ app.get("/urls/:id", (req, res) => {
     res.render("urls_show", templateVars);
 });
 
+//handle JSON reponse of URL database
 app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
   });
 
+// handle HTML response of URL database
 app.get("/hello", (req, res) => {
     res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+// add new URL to database
 app.post("/urls", (req, res) => {
     const shortURL = generateRandomString();
     const longURL = req.body.longURL;
@@ -53,6 +62,7 @@ app.post("/urls", (req, res) => {
     res.redirect(`/urls/${shortURL}`);
 });
 
+// redirect to long URL
 app.get("/u/:id", (req, res) => {
     const shortURL = req.params.id;
     const longURL = urlDatabase[shortURL];
@@ -63,9 +73,11 @@ app.get("/u/:id", (req, res) => {
     }
 });
 
+// delete URL from database
 app.post("/urls/:id/delete", (req, res) => {
     const urlID = req.params.id;
-    if (urlDatabase[urlID]) {
+    const longURL = urlDatabase[urlID];
+    if (longURL && longURL !== "") {
         delete urlDatabase[urlID];
         res.redirect("/urls");
     } else {
@@ -74,10 +86,23 @@ app.post("/urls/:id/delete", (req, res) => {
   
 });
 
+// update URL in database
 app.post("/urls/:id", (req, res) => {
     const urlID = req.params.id;
     const newURL = req.body.newLongURL;
+    if (newURL === "") {
+        if (urlDatabase[urlID]) {
+            res.redirect("/urls");
+        } else {    
+            res.status(404).send("URL not found");
+        }   
+    } else {
+        if (urlDatabase[urlID]) {
     urlDatabase[urlID] = newURL;
-    console.log("Updated URL Database:", urlDatabase);
     res.redirect("/urls");
+    } else {
+    res.status(404).send("URL not found");
+    }
+}
+    
 });
