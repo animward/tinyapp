56307b26e6866f8235b7821bd8fc4916ed47384b
animward <animward@gmail.com>
2024-03-19 13:58:57 -0700

    login feature and aligned to right

--- express_server.js
+++ express_server.js
@@ -106,3 +106,11 @@ app.post("/urls/:id", (req, res) => {
 }
     
 });
+
+//login
+
+app.post("/login", (req, res) => {
+    const username = req.body.username;
+    res.cookie('username', username);
+    res.redirect("/urls");
+});
